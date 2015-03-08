bcdcfa7c5e246676ae03764bfbdbff5e5e4f94c3
Robert Kohr <robkohr@gmail.com>
2015-03-08 09:40:48 -0400

    added car motion, reflection, and built APK

---
+++ Assets/Scripts/CarControlScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var WheelFL: WheelCollider;
+var WheelFR: WheelCollider;
+var WheelRL: WheelCollider;
+var WheelRR: WheelCollider;
+var maxTorque: float = 50;
+function Start () {
+	rigidbody.centerOfMass.y = -0.9;
+}
+
+function FixedUpdate () {
+	WheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	WheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	WheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
+	WheelFR.steerAngle = 10 * Input.GetAxis("Horizontal");
+} 
